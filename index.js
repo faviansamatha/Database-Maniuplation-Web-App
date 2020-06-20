e5d615224edcc58e8ab1aba6ca12431ae62af280
Favian Samatha <faviansamatha@yahoo.com>
2020-06-20 01:21:49 -0700

    Changed the front end of the website with a lot of css. Used bootstrap for the navbar and I started adding stuff related to the database but not much

--- index.js
+++ index.js
@@ -1,30 +1,56 @@
 const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const path = require('path');
 const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const PORT = process.env.PORT || 5000
+var pool;
+pool = new Pool({
+  connectionString: 'postgres://postgres:Ianmicro32@localhost/users'
+})
+
+// const { Pool } = require('pg');
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// })
+// ;
 // postgresql-rigid-22557
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+  // .get('/db', async (req, res) => {
+  //   try {
+  //     const client = await pool.connect();
+  //     const result = await client.query('SELECT * FROM test_table');
+  //     const results = { 'results': (result) ? result.rows : null};
+  //     res.render('pages/db', results );
+  //     client.release();
+  //   } catch (err) {
+  //     console.error(err);
+  //     res.send("Error " + err);
+  //   }
+  // })
+app.get('/database', (req,res)=>{
+
+  // var data = {results: [2,3,4,5,6]};
+  // res.render('pages/db',data)
+  // var data = {results: [2,3,4,5,6]};
+  var getUsersQuery = `SELECT * FROM usr`;
+  pool.query(getUsersQuery, (error, results) =>{
+    if (error)
+      res.end(error);
+    var results = {'rows':results.rows}
+    res.render('pages/db',results);
+  }
+  
+  )
+
+  
+})
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
